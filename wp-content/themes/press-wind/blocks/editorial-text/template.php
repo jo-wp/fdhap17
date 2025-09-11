00e9677d564c7eb8ede89f92073c8be284f8cecd
clairemugnier <contact@clairemugnier.fr>
2025-09-11 15:22:24 +0200

    Editorial text block : add option block (one col)

--- wp-content/themes/press-wind/blocks/editorial-text/template.php
+++ wp-content/themes/press-wind/blocks/editorial-text/template.php
@@ -7,9 +7,10 @@
 
 //ACF FIELDS
 $content_texte_editorial = get_field('content_texte_editorial');
+$content_one_col = get_field('one_col');
 
 // INNERBLOCKS
-$allowedBlocks = ['core/heading', 'core/paragraph'];
+$allowedBlocks = ['core/heading'];
 $template = [
 	[
 		'core/heading',
@@ -18,25 +19,27 @@ $template = [
 			"level" => 2,
       "color" => "foreground"
 		]
-	],
-	[
-		'core/paragraph',
-		[
-			"placeholder" => "Description ..."
-		]
 	]
 ];
 
-
+if($content_one_col) {
+    $class = "max-w-[1000px]  text-center mb-[50px]";
+    $class_container = "";
+    $class_text = "mb-[20px] [&_p]:text-center";
+} else {
+    $class = 'max-w-[1280px] flex max-md:flex-col md:flex-row flex-wrap justify-between items-start md:gap-[70px]';
+    $class_container = "flex-1 md:max-w-[40%]";
+    $class_text = "mb-[40px] after-underline";
+}
 
 
 ?>
-<section <?= get_block_wrapper_attributes(["class" => 'block-editorial-text max-w-[1280px] mx-auto max-[1280px]:mx-[30px] flex max-md:flex-col md:flex-row flex-wrap justify-between items-start md:gap-[70px]']); ?>>
-  <div class="flex-1 md:max-w-[40%]">
-      <InnerBlocks class="mb-[40px]  [&_p]:text-[14px] md:[&_p]:text-[15px] [&_h2]:relative max-md:[&_h2]:text-center  [&_h2::after]:block [&_h2]:text-orange [&_h2]:text-[20px]  md:[&_h2]:text-[30px] [&_h2]:font-[700] [&_h2_span]:font-arial [&_h2_span]:text-[32px] [&_h2_span]:font-[400] after-underline" template="<?php echo esc_attr(wp_json_encode($template)) ?>"
+<section <?= get_block_wrapper_attributes(["class" => 'block-editorial-text  mx-auto max-[1280px]:mx-[30px] '.$class.' ']); ?>>
+  <div class="<?php echo $class_container ?>">
+      <InnerBlocks class="[&_h2]:relative max-md:[&_h2]:text-center  [&_h2::after]:block [&_h2]:text-orange [&_h2]:text-[20px]  md:[&_h2]:text-[30px] [&_h2]:font-[700] [&_h2_span]:font-arial [&_h2_span]:text-[32px] [&_h2_span]:font-[400]  <?php echo $class_text ?>" template="<?php echo esc_attr(wp_json_encode($template)) ?>"
         allowedBlocks="<?php echo esc_attr(wp_json_encode($allowedBlocks)) ?>" templateLock="all" />
   </div>
-  <div class="flex-1 max-md:[&_p]:text-center [&_p]:m-0 [&_p]:text-[15px] [&_p]:text-black [&_p]:font-[400]">
+  <div class="flex-1 max-md:[&_p]:text-center [&_p]:m-0 [&_p:not(:last-child)]:mb-[28px] [&_p]:text-[15px] [&_p]:text-black [&_p]:font-[400]">
     <?= $content_texte_editorial; ?>
   </div>
 </section>
