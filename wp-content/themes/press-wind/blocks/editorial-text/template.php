29e282c8cdf2554164fb5b85f05307e70ce9bed2
clairemugnier <contact@clairemugnier.fr>
2025-09-12 14:50:12 +0200

    block editorial-text fix css

--- wp-content/themes/press-wind/blocks/editorial-text/template.php
+++ wp-content/themes/press-wind/blocks/editorial-text/template.php
@@ -27,14 +27,14 @@ if($content_one_col) {
     $class_container = "";
     $class_text = "mb-[20px] [&_p]:text-center";
 } else {
-    $class = 'max-w-[1280px] flex max-md:flex-col md:flex-row flex-wrap justify-between items-start md:gap-[70px]';
-    $class_container = "flex-1 md:max-w-[40%]";
+    $class = 'max-w-[1260px] px-[15px] md:px-[30px] flex max-md:flex-col md:flex-row flex-wrap justify-between items-start md:gap-[70px]';
+    $class_container = "w-full md:max-w-[40%]";
     $class_text = "mb-[40px] after-underline";
 }
 
 
 ?>
-<section <?= get_block_wrapper_attributes(["class" => 'block-editorial-text  mx-auto max-[1280px]:mx-[30px] '.$class.' ']); ?>>
+<section <?= get_block_wrapper_attributes(["class" => 'block-editorial-text  mx-auto '.$class.' ']); ?>>
   <div class="<?php echo $class_container ?>">
       <InnerBlocks class="[&_h2]:relative max-md:[&_h2]:text-center  [&_h2::after]:block [&_h2]:text-orange [&_h2]:text-[20px]  md:[&_h2]:text-[30px] [&_h2]:font-[700] [&_h2_span]:font-arial [&_h2_span]:text-[32px] [&_h2_span]:font-[400]  <?php echo $class_text ?>" template="<?php echo esc_attr(wp_json_encode($template)) ?>"
         allowedBlocks="<?php echo esc_attr(wp_json_encode($allowedBlocks)) ?>" templateLock="all" />
