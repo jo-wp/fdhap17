7f7f4367f89643333959153346dd7fc8401c2da8
clairemugnier <contact@clairemugnier.fr>
2025-09-12 08:58:30 +0200

    Add block buttons

--- wp-content/themes/press-wind/blocks/minisite-cards/template.php
+++ wp-content/themes/press-wind/blocks/minisite-cards/template.php
@@ -12,6 +12,7 @@ $styleM = ucfirst($style);
 
 
 
+
 // INNERBLOCKS
 $allowedBlocks = ['core/heading'];
 $template = [
@@ -25,7 +26,7 @@ $template = [
   ],
 ];
 ?>
-<section <?= get_block_wrapper_attributes(["class" => 'container-huge block-minisite-card relative md:after:rounded-[20px] md:after:bottom-[-50px] md:after:left-0 md:after:z-10 md:after:content-[""] md:after:absolute md:after:bg-bg'.$styleM.' md:after:w-full md:after:h-[30%] mb-[150px]']); ?>>
+<section <?= get_block_wrapper_attributes(["class" => 'block-minisite-card relative md:after:rounded-[20px] md:after:bottom-[-50px] md:after:left-0 md:after:z-10 md:after:content-[""] md:after:absolute md:after:bg-bg'.$styleM.' md:after:w-full md:after:h-[30%] mb-[150px]']); ?>>
   <div class="max-w-[1650px] mx-auto style-<?php echo $style ?>">
     <InnerBlocks class="[&_h2]:text-<?php echo $style ?> [&_h2]:text-center [&_h2]:text-[32px]"
       template="<?php echo esc_attr(wp_json_encode($template)) ?>"
@@ -42,16 +43,21 @@ $template = [
                     <?php while( have_rows('item') ): the_row();
                         $title = get_sub_field('title');
                         $image  = get_sub_field('image');
+                        $link = get_sub_field( 'link' );
+                        $link_e = get_sub_field( 'link_extern' );
+                        if($link_e) {
+                            $link = get_sub_field( 'url' );
+                        }
                         ?>
-                        <li class="">
-                            <div
+                        <li>
+                            <a href="<?php echo $link ?>" <?php if($link_e) { ?>target="_blank" <?php } ?>
                                     class="relative max-md:shadow-md h-[480px] bg-cover box-border after:content-[''] after:rounded-[10px] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[50%] after:bg-gradient-to-b after:from-transparent after:to-[#00000066] rounded-[10px] flex flex-row justify-start items-end md:pl-5 md:pb-5"
                                     style="background-image: url('<?php echo $image["url"]; ?>)">
                                 <h3 class="text-white text-[20px] font-bold font-arial m-0 relative z-10 mb-[10px] max-md:rounded-b-[10px] max-md:flex max-md:flex-col max-md:gap-1 max-md:items-center max-md:justify-center max-md:text-black max-md:text-[16px] max-md:bg-white max-md:w-full max-md:text-center max-md:h-[65px] max-md:mb-0">
                                     <?php echo $title ?>
 
                                 </h3>
-                            </div>
+                            </a>
 
                         </li>
                     <?php endwhile; ?>
