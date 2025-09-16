120e05bb3fc83488b1da0dafe4801fbe45506f43
Jordan <jordan.morand@gmail.com>
2025-09-16 16:58:14 +0200

    recette (seo) camping/destination

--- wp-content/themes/press-wind/blocks/editorial-text/template.php
+++ wp-content/themes/press-wind/blocks/editorial-text/template.php
@@ -36,8 +36,13 @@ if($content_one_col) {
 ?>
 <section <?= get_block_wrapper_attributes(["class" => 'block-editorial-text  mx-auto '.$class.' ']); ?>>
   <div class="<?php echo $class_container ?>">
-      <InnerBlocks class="[&_h2]:relative max-md:[&_h2]:text-center  [&_h2::after]:block [&_h2]:text-orange [&_h2]:text-[20px]  md:[&_h2]:text-[30px] [&_h2]:font-[700] [&_h2_span]:font-arial [&_h2_span]:text-[32px] [&_h2_span]:font-[400]  <?php echo $class_text ?>" template="<?php echo esc_attr(wp_json_encode($template)) ?>"
-        allowedBlocks="<?php echo esc_attr(wp_json_encode($allowedBlocks)) ?>" templateLock="all" />
+      <InnerBlocks class="[&_h2]:relative max-md:[&_h2]:text-center [&_h1]:relative max-md:[&_h1]:text-center 
+      [&_h2::after]:block [&_h2]:text-orange [&_h2]:text-[20px] [&_h1::after]:block [&_h1]:text-orange [&_h1]:text-[20px]
+      md:[&_h2]:text-[30px] [&_h2]:font-[700] [&_h2_span]:font-arial [&_h2_span]:text-[32px] [&_h2_span]:font-[400] 
+       md:[&_h1]:text-[30px] [&_h1]:font-[700] [&_h1_span]:font-arial [&_h1_span]:text-[32px] [&_h1_span]:font-[400]
+      <?php echo $class_text ?>" 
+      template="<?php echo esc_attr(wp_json_encode($template)) ?>"
+      allowedBlocks="<?php echo esc_attr(wp_json_encode($allowedBlocks)) ?>" templateLock="all" />
   </div>
   <div class="flex-1 max-md:[&_p]:text-center [&_p]:m-0 [&_p:not(:last-child)]:mb-[28px] [&_p]:text-[15px] [&_p]:text-black [&_p]:font-[400] [&_a]:underline">
     <?= $content_texte_editorial; ?>
