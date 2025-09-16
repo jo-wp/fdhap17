4f8f2fe31cca8f6a92e93d2a040e48c1df697473
Jordan <jordan.morand@gmail.com>
2025-09-16 15:49:35 +0200

    recette (seo) camping/destination

--- wp-content/themes/press-wind/blocks/presentation/template.php
+++ wp-content/themes/press-wind/blocks/presentation/template.php
@@ -112,13 +112,13 @@ if ($type_block_presentation == 'default') {
       endif; ?>
     </div>
     <div class="flex-1 <?= $block_texte_class; ?>  max-md:px-[15px] lg:pr-[100px]">
-      <InnerBlocks class="[&_h2]:leading-[36px]
-        [&_h2]:mb-[50px] [&_h2]:text-black [&_li]:mt-0 max-md:[&_li]:text-[14px]
+      <InnerBlocks class="[&_h2]:leading-[36px] [&_h1]:leading-[36px]
+        [&_h2]:mb-[50px] [&_h1]:mb-[50px] [&_h2]:text-black [&_h1]:text-black [&_li]:mt-0 max-md:[&_li]:text-[14px]
         md:[&_li]:text-[16px] [&_li]:text-[#333333] [&_li]:font-arial [&_p]:mt-0 text-[14px] 
-        md:[&_p]:text-[16px] [&_p]:text-[#333333] [&_p]:font-arial [&_p_a]:underline md:[&_h2]:text-[36px] 
-        [&_h2]:font-[600] [&_h2]:font-ivymode [&_h2_sub]:font-arial  [&_h2_sub]:font-[400]
-         max-md:[&_h2]:text-center [&_h2_sub]:text-[24px] 
-         md:[&_h2_sub]:text-[32px]" template="<?= htmlspecialchars(json_encode($template)); ?>"
+        md:[&_p]:text-[16px] [&_p]:text-[#333333] [&_p]:font-arial [&_p_a]:underline md:[&_h2]:text-[36px] md:[&_h1]:text-[36px] 
+        [&_h2]:font-[600] [&_h1]:font-[600] [&_h2]:font-ivymode [&_h1]:font-ivymode [&_h2_sub]:font-arial [&_h1_sub]:font-arial  [&_h2_sub]:font-[400] [&_h1_sub]:font-[400]
+         max-md:[&_h2]:text-center [&_h2_sub]:text-[24px] max-md:[&_h1]:text-center [&_h1_sub]:text-[24px] 
+         md:[&_h2_sub]:text-[32px] md:[&_h1_sub]:text-[32px]" template="<?= htmlspecialchars(json_encode($template)); ?>"
         allowedBlocks="<?= htmlspecialchars(json_encode($allowedBlocks)); ?>" />
     </div>
   </div>
