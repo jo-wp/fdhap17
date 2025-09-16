3fe45f8d53bfe639d30331a73636e692a5bffa32
Jordan <jordan.morand@gmail.com>
2025-09-16 10:42:56 +0200

    recette (seo) camping/destination

--- wp-content/themes/press-wind/blocks/editorial/template.php
+++ wp-content/themes/press-wind/blocks/editorial/template.php
@@ -40,7 +40,7 @@ switch ($type_block_editorial) {
         $size_blocks_content = ' md:flex-1 ';
         $size_blocks_image = ' md:flex-1 ';
 		$size_image = 'aspect-square lg:aspect-auto lg:min-w-[550px] w-full lg:min-h-[550px]';
-        $class_innerblock = '[&_p]:text-left [&_h2]:text-left  mb-[40px] [&_h2]:mb-[50px]';
+        $class_innerblock = '[&_p]:text-left [&_h2]:text-left [&_h1]:text-left  mb-[40px] [&_h2]:mb-[50px] [&_h1]:mb-[50px]';
 		$class_block_editorial = 'flex flex-col md:flex-row gap-0 md:gap-[53px]';
 		break;
 	case 'image_big_right':
@@ -48,7 +48,7 @@ switch ($type_block_editorial) {
         $size_blocks_content = 'max-md:w-full md:w-[50%] ';
         $size_blocks_image = 'max-md:w-full md:w-[50%] ';
 		$size_image = 'max-md:min-w-full md:min-w-[720px] w-full md:min-h-[720px] object-cover object-center';
-        $class_innerblock = '[&_p]:text-left [&_h2]:text-left  mb-[40px] [&_h2]:mb-[50px]';
+        $class_innerblock = '[&_p]:text-left [&_h2]:text-left [&_h1]:text-left  mb-[40px] [&_h2]:mb-[50px] [&_h1]:mb-[50px]';
 		$class_block_editorial_texte = '-m-[50px] bg-white px-[73px] py-[100px] rounded-[10px] shadow-[1px_8px_30px_0_rgba(186,186,186,0.18)]';
 		$class_block_editorial = 'flex max-md:flex-col md:flex-row-reverse ';
 		break;
@@ -56,7 +56,7 @@ switch ($type_block_editorial) {
         $size_section = 'max-w-[1260px] max-md:mx-[15px] max-xl:mx-[30px] ';
         $size_blocks_content = 'max-md:w-full md:w-[52%] ';
         $size_blocks_image = 'max-md:w-full md:w-[65%] ';
-        $class_innerblock = '[&_p]:text-center [&_h2]:text-center [&_a]:underline mb-0 [&_h2]:mb-[35px]';
+        $class_innerblock = '[&_p]:text-center [&_h2]:text-center [&_h1]:text-center [&_a]:underline mb-0 [&_h2]:mb-[35px] [&_h1]:mb-[35px]';
         $size_image = 'w-full h-full object-cover object-center';
         $class_block_editorial_texte = 'md:-ml-[17%] bg-white px-[15px] lg:px-[62px] py-[70px] rounded-[10px] shadow-[1px_8px_30px_0_rgba(186,186,186,0.18)]';
         $class_block_editorial = 'flex max-md:flex-col md:flex-row-reverse ';
@@ -69,7 +69,7 @@ switch ($type_block_editorial) {
 
 <section <?= get_block_wrapper_attributes(["class" => 'block-editorial '.$size_section.' mx-auto '.$class_block_editorial.'   justify-between items-center']); ?>>
   <div class="<?= $size_blocks_content; ?> <?= $class_block_editorial_texte; ?>" >
-      <InnerBlocks class="<?php echo $class_innerblock; ?> md:[&_p]:text-inherit md:[&_h2]:text-inherit [&_h2_sub]:text-center md:[&_h2_sub]:text-inherit text-[14px] md:[&_p]:text-[15px] [&_h2]:font-[600] [&_h2]:text-[24px] md:[&_h2]:text-[32px] [&_h2_sub]:font-arial [&_h2_sub]:text-[24px] md:[&_h2_sub]:text-[32px] [&_h2_sub]:float-none  md:[&_h2_sub]:float-right [&_h2_sub]:font-[400] [&_h2]:font-[400] [&_p]:font-arial" template="<?php echo esc_attr(wp_json_encode($template)) ?>"
+      <InnerBlocks class="<?php echo $class_innerblock; ?> md:[&_p]:text-inherit md:[&_h2]:text-inherit md:[&_h1]:text-inherit [&_h2_sub]:text-center [&_h1_sub]:text-center md:[&_h2_sub]:text-inherit md:[&_h1_sub]:text-inherit text-[14px] md:[&_p]:text-[15px] [&_h2]:font-[600] [&_h1]:font-[600] [&_h2]:text-[24px] [&_h1]:text-[24px] md:[&_h2]:text-[32px] md:[&_h1]:text-[32px] [&_h2_sub]:font-arial [&_h1_sub]:font-arial [&_h2_sub]:text-[24px] [&_h1_sub]:text-[24px] md:[&_h2_sub]:text-[32px] md:[&_h1_sub]:text-[32px] [&_h2_sub]:float-none [&_h1_sub]:float-none  md:[&_h2_sub]:float-right md:[&_h1_sub]:float-right [&_h2_sub]:font-[400] [&_h1_sub]:font-[400] [&_h2]:font-[400] [&_h1]:font-[400] [&_p]:font-arial" template="<?php echo esc_attr(wp_json_encode($template)) ?>"
         allowedBlocks="<?php echo esc_attr(wp_json_encode($allowedBlocks)) ?>" templateLock="all" />
   </div>
   <div class="<?= $size_blocks_image; ?>">
