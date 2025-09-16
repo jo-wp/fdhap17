1ba4300a2c789f3be7f983d7a5d6ce761590cc4f
Jordan <jordan.morand@gmail.com>
2025-09-16 10:51:38 +0200

    recette (seo) camping/destination

--- wp-content/themes/press-wind/blocks/pannels/template.php
+++ wp-content/themes/press-wind/blocks/pannels/template.php
@@ -36,7 +36,7 @@ $template = [
 <section <?= get_block_wrapper_attributes(["class" => 'block-pannels max-w-[1066px] mx-auto flex flex-col flex-wrap']); ?>>
   <div>
     <InnerBlocks
-      class=" [&_h2]:text-black [&_h2_sub]:text-black [&_p]:m-0 [&_p]:text-[20px] md:[&_p]:text-[32px] [&_p]:font-[400] [&_p]:text-black [&_p]:font-arial [&_h2]:text-[20px] [&_h2_sub]:text-[20px] md:[&_h2]:text-[36px] md:[&_h2_sub]:text-[36px] [&_h2]:font-[700] [&_h2_sub]:font-[700] [&_h2]:font-ivymode [&_h2_sub]:font-arial"
+      class=" [&_h2]:text-black [&_h2_sub]:text-black [&_p]:m-0 [&_p]:text-[20px] md:[&_p]:text-[32px] [&_p]:font-[400] [&_p]:text-black [&_p]:font-arial [&_h2]:text-[20px] [&_h2_sub]:text-[20px] md:[&_h2]:text-[36px] md:[&_h2_sub]:text-[32px] [&_h2]:font-[700] [&_h2_sub]:font-[400] [&_h2]:font-ivymode [&_h2_sub]:font-arial"
       template="<?= htmlspecialchars(json_encode($template)); ?>"
       allowedBlocks="<?= htmlspecialchars(json_encode($allowedBlocks)); ?>" />
   </div>
