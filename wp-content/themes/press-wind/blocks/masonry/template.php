29eaa9bc7bafe4156b21dc62e4817d916a98880d
Jordan <jordan.morand@gmail.com>
2025-09-17 16:20:44 +0200

    recette seo : add under title in span & acf field

--- wp-content/themes/press-wind/blocks/masonry/template.php
+++ wp-content/themes/press-wind/blocks/masonry/template.php
@@ -7,7 +7,34 @@
  */
 
 //ACF FIELDS
-$items_associated = get_field('items_associated');
+$disabled_auto_mansory = get_field('disabled_auto_mansory');
+$select_cat_mansory = get_field('select_cat_mansory');
+
+$items_associated = '';
+if (!$disabled_auto_mansory) {
+  $q = new WP_Query([
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'posts_per_page' => 5,
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'tax_query' => [
+      [
+        'taxonomy' => 'category',
+        'field' => 'term_id',
+        'terms' => $select_cat_mansory,
+      ]
+    ],
+  ]);
+
+  if ($q->have_posts()) {
+    $items_associated = $q->posts;
+  }
+  wp_reset_postdata();
+} else {
+  $items_associated = get_field('items_associated');
+}
+
 
 // INNERBLOCKS
 $allowedBlocks = ['core/heading', 'core/paragraph'];
@@ -36,13 +63,19 @@ $template = [
       template="<?= htmlspecialchars(json_encode($template)); ?>"
       allowedBlocks="<?= htmlspecialchars(json_encode($allowedBlocks)); ?>" />
   </div>
-  <div class="md:grid grid-mansonyried max-md:grid-cols-2 max-md:flex max-md:overflow-x-scroll max-md:gap-[15px] max-md:ml-[15px]">
+  <div
+    class="md:grid grid-mansonyried max-md:grid-cols-2 max-md:flex max-md:overflow-x-scroll max-md:gap-[15px] max-md:ml-[15px]">
     <?php foreach ($items_associated as $post):
       $featuredImage = get_the_post_thumbnail_url($post->ID, 'full');
-    ?>
-      <article class="post relative after:rounded-[20px] after:z-10 after:w-full after:h-full after:absolute after:content-[''] after:top-0 after:left-0 min-h-[430px] rounded-[20px] bg-cover bg-center max-md:min-w-[340px]" style="background-image: url('<?= esc_url($featuredImage); ?>');">
-        <a class="hover:no-underline w-full h-full flex items-end relative z-20 hover:translate-x-2 transition-all duration-300 " href="<?= esc_url(get_permalink($post->ID)); ?>">
-          <h3 class="post-title m-0 p-0 font-arial text-white text-center text-[20px] font-[700] mb-[42px] ml-[36px] border border-solid border-white py-[8px] px-[19px] rounded-[40px]"><?= esc_html($post->post_title); ?></h3>
+      ?>
+      <article
+        class="post relative after:rounded-[20px] after:z-10 after:w-full after:h-full after:absolute after:content-[''] after:top-0 after:left-0 min-h-[430px] rounded-[20px] bg-cover bg-center max-md:min-w-[340px]"
+        style="background-image: url('<?= esc_url($featuredImage); ?>');">
+        <a class="hover:no-underline w-full h-full flex items-end relative z-20 hover:translate-x-2 transition-all duration-300 "
+          href="<?= esc_url(get_permalink($post->ID)); ?>">
+          <h3
+            class="post-title m-0 p-0 font-arial text-white text-center text-[20px] font-[700] mb-[42px] ml-[36px] border border-solid border-white py-[8px] px-[19px] rounded-[40px]">
+            <?= esc_html($post->post_title); ?></h3>
         </a>
       </article>
     <?php endforeach; ?>
