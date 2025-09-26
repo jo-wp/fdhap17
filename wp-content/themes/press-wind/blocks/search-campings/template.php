e6b341578b9c2a70f39573dcec788543835e81be
Jordan <jordan.morand@gmail.com>
2025-09-26 14:01:22 +0200

    edit : responsive

--- wp-content/themes/press-wind/blocks/search-campings/template.php
+++ wp-content/themes/press-wind/blocks/search-campings/template.php
@@ -31,7 +31,7 @@ $template = [
 
 
 <section <?= get_block_wrapper_attributes(["class" => 'block-search-campings container-huge bg-bgGreen rounded-t-[200px] mx-auto flex flex-col flex-wrap ']); ?>>
-  <div class="max-w-[1066px] mx-auto flex flex-col flex-wrap py-[90px] px-[130px]">
+  <div class="max-w-[1066px] mx-auto flex flex-col flex-wrap py-[90px] px-[60px] md:px-[130px]">
     <InnerBlocks
       class="animateFade fadeOutAnimation text-center [&_h2]:text-green [&_h2_sub]:text-black [&_p]:m-0 [&_p]:text-[16px] [&_p]:font-[400] [&_p]:text-black [&_p]:font-arial [&_h2]:text-[32px] [&_h2_sub]:text-[32px] md:[&_h2]:text-[32px] md:[&_h2_sub]:text-[32px] [&_h2]:font-[600] [&_h2_sub]:font-[400] [&_h2]:font-ivymode [&_h2_sub]:font-arial"
       template="<?= htmlspecialchars(json_encode($template)); ?>"
@@ -43,7 +43,7 @@ $template = [
         <?= do_shortcode('[facetwp facet="classement_block"]'); ?>
         <div data-button="map" class="button-map cursor-pointer flex flex-row flex-wrap justify-center hover:bg-green transition-all items-center gap-2 bg-orange rounded-[50px] px-[27px] py-[15px]">
           <img src="<?= get_bloginfo('template_directory') ?>/assets/media/icon-map.svg" alt="Icon map">
-          <span class="font-arial text-[14px] font-[700] text-white">Voir sur la carte</span>
+          <span class="font-arial text-[14px] font-[700] text-white"><?= __('Voir sur la carte','fdhpa17'); ?></span>
         </div>
       </div>
         <?= do_shortcode('[facetwp template="block_search"]'); ?>
