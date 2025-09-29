438dbe2050a06740e6db14fdcd70885ae8c9ceef
Jordan <jordan.morand@gmail.com>
2025-09-29 17:27:13 +0200

    edit : responsive

--- wp-content/themes/press-wind/blocks/search-campings/template.php
+++ wp-content/themes/press-wind/blocks/search-campings/template.php
@@ -33,7 +33,8 @@ $template = [
 <section <?= get_block_wrapper_attributes(["class" => 'block-search-campings container-huge bg-bgGreen rounded-t-[200px] mx-auto flex flex-col flex-wrap ']); ?>>
   <div class="max-w-[1066px] mx-auto flex flex-col flex-wrap max-md:pt-[60px] max-md:pb-[30px] md:py-[90px] px-[60px] md:px-[130px]">
     <InnerBlocks
-      class="animateFade fadeOutAnimation text-center [&_h2]:text-green [&_h2_sub]:text-black [&_p]:m-0 [&_p]:text-[14px] md:[&_p]:text-[16px] [&_p]:font-[400] [&_p]:text-black [&_p]:font-arial [&_h2]:text-[20px] md:[&_h2]:text-[32px] [&_h2_sub]:text-[20px]  md:[&_h2_sub]:text-[32px] [&_h2]:font-[600] [&_h2_sub]:font-[400] [&_h2]:font-ivymode [&_h2_sub]:font-arial"
+      class="animateFade fadeOutAnimation text-center [&_h2]:text-green [&_h2_sub]:text-black [&_p]:m-0 [&_p]:text-[14px] md:[&_p]:text-[16px] [&_p]:font-[400] [&_p]:text-black [&_p]:font-arial [&_h2]:text-[20px] md:[&_h2]:text-[32px] [&_h2_sub]:text-[20px]  md:[&_h2_sub]:text-[32px] [&_h2]:font-[600] [&_h2_sub]:font-[400] [&_h2]:font-ivymode [&_h2_sub]:font-arial
+      [&_h2]:mb-[20px]"
       template="<?= htmlspecialchars(json_encode($template)); ?>"
       allowedBlocks="<?= htmlspecialchars(json_encode($allowedBlocks)); ?>" />
   </div>
