c06bdccd233e33f2d3bfdae76026f19a7b1769f0
Jordan <jordan.morand@gmail.com>
2025-09-30 08:11:51 +0200

    edit : responsive

--- wp-content/themes/press-wind/assets/js/instagram/main.js
+++ wp-content/themes/press-wind/assets/js/instagram/main.js
@@ -1,11 +1,18 @@
 function instagramOverride(){
-  const instagramSection = document.querySelector('.instagram-gallery-list');
-  if(instagramSection){
-    console.log('exec')
-    const template = `<a class="instagram-gallery-item instagram-gallery-item--cols-3 ">
-    <h2 class="text-orange text-[32px] flex items-end justify-center text-right flex-col">Suivez-l'actualité <sub class="font-arial font-[400] text-[32px]">de Campings17</sub></h2>
-    </a>`;
-    instagramSection.insertAdjacentHTML('afterbegin',template);
-  }
+  setTimeout(() => {
+    const instagramSection = document.querySelector('.instagram-gallery-list');
+    if(instagramSection){
+      console.log('exec');
+      const template = `<a class="instagram-gallery-item instagram-gallery-item--cols-3 ">
+        <h2 class="text-orange text-[32px] flex items-end justify-center text-right flex-col">
+          Suivez-l'actualité 
+          <sub class="font-arial font-[400] text-[32px]">de Campings17</sub>
+        </h2>
+      </a>`;
+      instagramSection.insertAdjacentHTML('afterbegin', template);
+    } else {
+    }
+  }, 2000); // 2000 ms = 2 sec
 }
+
 export default instagramOverride;
