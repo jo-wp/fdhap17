e25599bcbd6e3f0fa6d57e04dc4640b1f1721879
Jordan <jordan.morand@gmail.com>
2025-10-15 18:29:07 +0200

    override: get post thumbnail url for camping if not image

--- wp-content/themes/press-wind/inc/override.php
+++ wp-content/themes/press-wind/inc/override.php
@@ -1,18 +1,10 @@
-<?php 
-
-add_filter('post_thumbnail_url', function($url, $post_id, $size) {
-
-    // Vérifie si on est sur un CPT ou une catégorie "camping"
-    $post_type = get_post_type($post_id);
-
-    // Ici, adapte selon ta logique : si ton CPT s'appelle "camping"
-    if ($post_type === 'camping') {
-
-        // Si pas d'image à la une, on renvoie celle du thème
-        if (empty($url)) {
-            $url = get_stylesheet_directory_uri() . '/assets/media/image-camping-sans-photo.png';
-        }
+<?php
+add_filter('post_thumbnail_url', function ($url, $post_id, $size) {
+  $post_type = get_post_type($post_id);
+  if ($post_type === 'camping') {
+    if (empty($url)) {
+      $url = get_stylesheet_directory_uri() . '/assets/media/image-camping-sans-photo.png';
     }
-
-    return $url;
-}, 10, 3);
+  }
+  return $url;
+}, 999, 3);
