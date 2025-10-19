3bd300925a1846170377d1ffe914e95e615f869f
Jordan <jordan.morand@gmail.com>
2025-10-19 12:47:30 +0200

    template contact mise en formce css

--- wp-content/themes/press-wind/blocks/carousel/template.php
+++ wp-content/themes/press-wind/blocks/carousel/template.php
@@ -74,7 +74,7 @@ $template = [
 $block_id = $block['id'];
 
 ?>
-<section <?= get_block_wrapper_attributes(["class" => 'block-carousel  max-w-[1260px] mx-[90px] xl:mx-auto']); ?>>
+<section <?= get_block_wrapper_attributes(["class" => 'block-carousel  max-w-[1260px]  md:mx-[90px] xl:mx-auto']); ?>>
   <div class="">
     <InnerBlocks
       class="mb-[40px] [&_p]:text-[15px] [&_h2]:relative [&_h2]:text-center  [&_h2::after]:block [&_h2]:text-orange [&_h2]:text-[24px] md:[&_h2]:text-[32px] [&_h2]:font-[600] [&_h2_sub]:font-arial [&_h2_sub]:text-[32px] [&_h2_sub]:font-[400] "
@@ -97,12 +97,12 @@ $block_id = $block['id'];
           </button>
         </div>
         <div class="splide__track">
-          <ul class="splide__list">
+          <ul class="splide__list max-md:flex max-md:flex-row max-md:flex-nowrap max-md:gap-[15px] max-md:overflow-x-scroll max-md:w-full">
             <?php foreach ($items_selected as $item):
               $image_featured = get_the_post_thumbnail_url($item->ID, 'full');
               $url = ($disabled_auto_carousel)?  tp_get_term_url_by_term_page($item->ID) : get_permalink($item->ID) ;
               ?>
-              <li class="splide__slide h-[62vw] md:h-[40vw] lg:h-[25vw] lg:max-h-[385px]">
+              <li class="splide__slide h-[62vw] md:h-[40vw] lg:h-[25vw] lg:max-h-[385px] max-md:w-[70%] max-md:h-[320px]">
                 <div class="image_featured min-h-[calc(100%-40px)] bg-cover  bg-no-repeat rounded-[20px] relative"
                   style="background-image:url('<?= $image_featured ?>');">
                   <a class="absolute w-full h-full -bottom-[30px] md:-bottom-[43px] left-0 flex items-end justify-start hover:no-underline no-underline "
