47ce43fd317b8456ab7850e0f8055e9174658e73
Jordan <jordan.morand@gmail.com>
2025-11-05 20:12:52 +0100

    edit

--- wp-content/themes/press-wind/header.php
+++ wp-content/themes/press-wind/header.php
@@ -49,6 +49,9 @@
 		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 		<!-- End Google Tag Manager (noscript) -->
 
+		<!-- BING -->
+		<script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"17571109", enableAutoSpaTracking: true};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script>
+		<!-- End BING -->
 	<?php endif; ?>
 
 
