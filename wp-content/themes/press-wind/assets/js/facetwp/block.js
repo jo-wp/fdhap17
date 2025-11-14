0c8f6f98ab5a7b7a176cb8e1cfba9bef753142f1
Jordan <jordan.morand@gmail.com>
2025-11-14 14:32:51 +0100

    zindex firefox bug

--- wp-content/themes/press-wind/assets/js/facetwp/block.js
+++ wp-content/themes/press-wind/assets/js/facetwp/block.js
@@ -16,7 +16,7 @@ export default function blockSearch() {
             640: {
               perPage: 3,
               direction: 'ttb',
-              height: '1400px',
+              height: '1390px',
               wheel: false,
               fixedHeight: '450px',
               drag: false,
