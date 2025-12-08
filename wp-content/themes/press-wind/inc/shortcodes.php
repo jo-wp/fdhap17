1d120ce7ab61d617c663d3ed77b287a8abfac4e5
Jordan <jordan.morand@gmail.com>
2025-12-08 13:33:55 +0100

    refact

--- wp-content/themes/press-wind/inc/shortcodes.php
+++ wp-content/themes/press-wind/inc/shortcodes.php
@@ -12,3 +12,21 @@ function shortcode_featured_campings()
   // return '';
 }
 add_shortcode('featured_campings', 'shortcode_featured_campings');
+
+function camping_title_shortcode( $atts ) {
+    // Récupère le post courant
+    global $post;
+
+    if ( ! $post ) {
+        return '';
+    }
+
+    // Optionnel : on vérifie qu'on est bien sur un CPT "camping"
+    if ( get_post_type( $post ) !== 'camping' ) {
+        return '';
+    }
+
+    // Renvoie le title du camping
+    return get_the_title( $post );
+}
+add_shortcode( 'camping_title', 'camping_title_shortcode' );
