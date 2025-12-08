0d702a9b96e8ccaefcd878f15a87202d5e4b458e
Jordan <jordan.morand@gmail.com>
2025-12-08 13:41:32 +0100

    reac

--- wp-content/themes/press-wind/inc/shortcodes.php
+++ wp-content/themes/press-wind/inc/shortcodes.php
@@ -12,24 +12,3 @@ function shortcode_featured_campings()
   // return '';
 }
 add_shortcode('featured_campings', 'shortcode_featured_campings');
-
-add_filter('ninja_forms_render_default_value', function ($default, $field) {
-    // Vérifie si on est sur une page avec un post
-    if (!is_singular()) {
-        return $default;
-    }
-
-    // Clé de champ Ninja Forms
-    if ($field['key'] === 'hidden_1') {
-        return get_the_title();
-    }
-
-    return $default;
-}, 10, 2);
-
-add_filter('ninja_forms_merge_tag_data', function ($tags) {
-    if (is_singular()) {
-        $tags['post:title'] = get_the_title();
-    }
-    return $tags;
-});
