ceabcb5128f2c5aa365fed484464a2581031fa3d
Jordan <jordan.morand@gmail.com>
2026-01-08 15:51:58 +0100

    refactor trello JM

--- wp-content/themes/press-wind/assets/js/facetwp/mobile.js
+++ wp-content/themes/press-wind/assets/js/facetwp/mobile.js
@@ -1,37 +1,43 @@
 export default function filtreMobile() {
-  const wrapperFilters = document.querySelector('.wrapper-filters');
-  if (!wrapperFilters) return;
+  const wrapperFilters = document.querySelector('.wrapper-filters')
+  if (!wrapperFilters) return
 
-  const buttonFilters = document.querySelectorAll('[data-button="filters"]');
-  if (!buttonFilters.length) return;
+  const buttonFilters = document.querySelectorAll('[data-button="filters"]')
+  if (!buttonFilters.length) return
 
   buttonFilters.forEach((button) => {
     button.addEventListener('click', () => {
-      wrapperFilters.classList.toggle('active');
-    });
-  });
+      wrapperFilters.classList.toggle('active')
+    })
+  })
 
-  const filters = document.querySelector('.block-search .wrapper-filters');
+  const filters = document.querySelector('.block-search .wrapper-filters')
 
-filters.addEventListener('click', function(e) {
-  if (!filters.classList.contains('active')) return;
+  filters.addEventListener('click', function (e) {
+    if (!filters.classList.contains('active')) return
 
-  const rect = filters.getBoundingClientRect();
+    const rect = filters.getBoundingClientRect()
 
-  const closeX = window.innerWidth - 20 - 42;
-  const closeY = 20;
-  const size = 42;
+    const closeX = window.innerWidth - 20 - 42
+    const closeY = 20
+    const size = 42
 
-  const x = e.clientX;
-  const y = e.clientY;
+    const x = e.clientX
+    const y = e.clientY
 
-  if (
-    x >= closeX &&
-    x <= closeX + size &&
-    y >= closeY &&
-    y <= closeY + size
-  ) {
-    filters.classList.remove('active');
-  }
-});
+    if (
+      x >= closeX &&
+      x <= closeX + size &&
+      y >= closeY &&
+      y <= closeY + size
+    ) {
+      filters.classList.remove('active')
+    }
+  })
+
+  filters.addEventListener('click', function (e) {
+    if (e.target === filters) {
+      filters.classList.remove('active')
+    }
+  })
 }
