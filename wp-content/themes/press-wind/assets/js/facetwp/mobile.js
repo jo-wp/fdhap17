b37282f34854ec435eb2c672534040637d307de2
Jordan <jordan.morand@gmail.com>
2026-01-08 15:50:04 +0100

    refactor trello JM

--- wp-content/themes/press-wind/assets/js/facetwp/mobile.js
+++ wp-content/themes/press-wind/assets/js/facetwp/mobile.js
@@ -10,4 +10,28 @@ export default function filtreMobile() {
       wrapperFilters.classList.toggle('active');
     });
   });
+
+  const filters = document.querySelector('.block-search .wrapper-filters');
+
+filters.addEventListener('click', function(e) {
+  if (!filters.classList.contains('active')) return;
+
+  const rect = filters.getBoundingClientRect();
+
+  const closeX = window.innerWidth - 20 - 42;
+  const closeY = 20;
+  const size = 42;
+
+  const x = e.clientX;
+  const y = e.clientY;
+
+  if (
+    x >= closeX &&
+    x <= closeX + size &&
+    y >= closeY &&
+    y <= closeY + size
+  ) {
+    filters.classList.remove('active');
+  }
+});
 }
