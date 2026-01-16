74c742736c956d2ac1804a9255005d88a6680686
Jordan <jordan.morand@gmail.com>
2026-01-16 08:47:47 +0100

    refact

--- wp-content/themes/press-wind/partials/search/fields/destinations.php
+++ wp-content/themes/press-wind/partials/search/fields/destinations.php
@@ -1,52 +1,16 @@
 <?php if (!is_page('carte-camping') && !is_front_page()):
-
-$destinations_featured = [
-    'camping-royan' => 'Royan',
-    'camping-ile-oleron' => "Île d'Oléron",
-    'camping-ile-de-re' => "Île de Ré",
-    'camping-pays-du-cognac' => 'Pays du Cognac',
-    'camping-marais-poitevin' => 'Marais Poitevin',
-    'camping-la-rochelle' => 'La Rochelle',
-    'camping-rochefort' => 'Rochefort',
-    'camping-futuroscope' => 'Futuroscope',
-];
-
-?>
-
-<div class="facetwp-facet facetwp-facet-destination facetwp-type-dropdown" data-name="destination" data-type="dropdown">
-    <select class="facetwp-dropdown">
-        <option value="">Destination</option>
-
-        <?php foreach ($destinations_featured as $slug => $label): ?>
-
-            <?php
-            $query = new WP_Query([
-                'post_type'      => 'camping',
-                'posts_per_page' => 1,
-                'tax_query'      => [
-                    [
-                        'taxonomy' => 'destination',
-                        'field'    => 'slug',
-                        'terms'    => $slug,
-                    ],
-                ],
-            ]);
-
-            $count = $query->found_posts;
-            wp_reset_postdata();
-            ?>
-
-            <?php if ($count > 0): ?>
-                <option value="<?= $slug ?>">
-                    <?= $label ?> (<?= $count ?>)
-                </option>
-            <?php endif; ?>
-
-        <?php endforeach; ?>
-
-    </select>
-</div>
-
-<?php else: ?>
-    <?= do_shortcode('[facetwp facet="destination"]'); ?>
-<?php endif; ?>
+	$destinations_featured = ['camping-royan', 'camping-ile-oleron', 'camping-ile-de-re', 'camping-pays-du-cognac', 'camping-marais-poitevin', 'camping-la-rochelle', 'camping-rochefort', 'camping-futuroscope',]; ?>
+	<div class="facetwp-facet facetwp-facet-destination facetwp-type-dropdown" data-name="destination" data-type="dropdown">
+		<select class="facetwp-dropdown">
+			<option value="">Destination</option>
+			<option value="camping-royan">Royan (82)</option>
+			<option value="camping-ile-oleron">Île d'Oléron (62)</option>
+			<option value="camping-ile-de-re">Île de Ré (36)</option>
+			<option value="camping-pays-du-cognac">Pays du Cognac (18)</option>
+			<option value="camping-marais-poitevin">Marais Poitevin (13)</option>
+			<option value="camping-la-rochelle">La Rochelle (7)</option>
+			<option value="camping-rochefort">Rochefort (7)</option>
+			<option value="camping-futuroscope">Futuroscope (4)</option>
+		</select>
+	</div>
+<?php else: ?> <?= do_shortcode('[facetwp facet="destination"]'); ?> <?php endif; ?>
